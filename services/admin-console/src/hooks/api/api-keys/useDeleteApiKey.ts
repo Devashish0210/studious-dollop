fc89e2fa65bee8cc925925ed64da10bb3982fac8
Devashish Bhuwan Joshi <69790529+Devashish0210@users.noreply.github.com>
2026-01-06 02:55:49 +0530

    Delete services/admin-console directory

--- services/admin-console/src/hooks/api/api-keys/useDeleteApiKey.ts
+++
@@ -1,7 +0,0 @@
-import { API_URL } from '@/config'
-import useDelete from '@/hooks/api/generics/useDelete'
-
-export const useDeleteApiKey = <T = void>() => {
-  const deleteApiKey = useDelete<T>()
-  return (apiKeyId: string) => deleteApiKey(`${API_URL}/keys/${apiKeyId}`)
-}
